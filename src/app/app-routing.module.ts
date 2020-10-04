60704afc81d7fa27f66fdebdf9cdd856bc07e534
Munge Ryan <mungeryan@gmail.com>
2020-10-04 12:41:25 +0300

    routes to navigate through the page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ErrorComponent } from './error/error.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { UsersComponent } from './users/users.component';
+
 
 
 const routes: Routes = [
+  {path: "profile-user", component:UsersComponent},
+  {path: "reposit-search", component:RepositoryComponent},
   { path: '**', component:ErrorComponent},
 
-  {path: '', redirectTo:"/users", pathMatch:"full"},
+  {path: '', redirectTo:"/profile-user", pathMatch:"full"},
 ];
 
 @NgModule({
