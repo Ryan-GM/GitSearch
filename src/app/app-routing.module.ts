3060d15ccefa39131a66b540a7a5c2d3e4ad2280
Munge Ryan <mungeryan@gmail.com>
2020-10-04 09:31:34 +0300

    error path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', component:ErrorComponent},
+
+  {path: '', redirectTo:"/users", pathMatch:"full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
