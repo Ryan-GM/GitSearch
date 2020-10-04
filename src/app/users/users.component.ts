2cff5c0912c185a9f6f3a6d847027c3dac75c762
Munge Ryan <mungeryan@gmail.com>
2020-10-04 09:49:08 +0300

    calling the user service

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../users';
+import { Reposit } from '../repository';
+import { UserService } from '../userservice/user.service';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  user: User;
+  reposit: Reposit;
 
-  constructor() { }
+  constructor(public myService: UserService, private repositService: UserService) { }
 
-  ngOnInit(): void {
+  search(searchName){
+    this.myService.searchUser(searchName).then(
+      (success) => {
+        this.user = this.myService.foundUser;
+      },
+      (error) => {
+        console.log(error)
+      }
+    );
+    this.repositService.getReposit(searchName).then(
+      (results) => {
+        this.reposit=this.repositService.allReposit
+        console.log(this.reposit);
+      },
+      (error) => {
+        console.log(error);
+      }
+    )
+  }
+
+  ngOnInit() {
   }
 
 }
