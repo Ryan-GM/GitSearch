b89f61e9871f27fb0c72c508a262b9300bdf4abc
Munge Ryan <mungeryan@gmail.com>
2020-10-04 13:23:43 +0300

    containing the apikey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'd0deb0e1a82c91230c96938b32161836aeca00ab'
 };
 
 /*
