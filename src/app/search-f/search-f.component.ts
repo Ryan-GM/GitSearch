2f2dd5dd22744aee4e41c428a5028fa321992a66
Munge Ryan <mungeryan@gmail.com>
2020-10-04 12:51:45 +0300

    importing an eventemitter

--- src/app/search-f/search-f.component.ts
+++ src/app/search-f/search-f.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search-f',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchFComponent implements OnInit {
 
+  searchName:string;
+  @Output() searchOutput = new EventEmitter<any>()
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  search(){
+    this.searchOutput.emit(this.searchName);
+    this.searchName = "";
   }
 
 }
