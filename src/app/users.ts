95222cea28d60f495dc52ed95cde10ab16da210d
Munge Ryan <mungeryan@gmail.com>
2020-10-04 13:25:38 +0300

    addding login variable class

---
+++ src/app/users.ts
@@ -0,0 +1,6 @@
+export class Users {
+  constructor(public url:string, public login:string, public html_url:string,  public location:string, 
+     public public_reposit:number, public followers:number, public following:number, public avatar_url:string, public created_at: Date ){
+
+  }
+}
